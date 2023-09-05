ec4808fa6ad3c18f7add55ddd37eb2cee4fce973
Salah <salaheddinehanaoui@gmail.com>
2023-09-05 10:26:12 +0100

    finished establishing connection with database

---
+++ Mini Sas/src/Connection/JDBC.java
@@ -0,0 +1,19 @@
+package Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBC {
+    private static Connection con;
+    public static void main(String[] args) {
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "");
+            System.out.println(con);
+            System.out.println("Connection Success");
+        } catch (Exception e) {
+            System.out.println("Connection Failed");
+        }
+    }
+    public static Connection getConnection() {
+        return con;
+    }
+}
